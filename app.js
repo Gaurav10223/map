3772e3f73fdb84e2f9e77a4447c4fbae111b08b9
Gauavhd <painkaragauravsingh@gmail.com>
2025-01-15 23:00:11 +0530

    fix 3

--- app.js
+++ app.js
@@ -110,48 +110,49 @@
 
 
 
-
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-
 const path = require("path");
+
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.use(express.static('public')); // Assuming you have static assets in a "public" folder
+// Set the views directory and view engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.set("view engine","ejs");
-app.use(express.static(path.join(__dirname,"public"))); //ye apan images aur css us kar pae is liye
+// Serve static assets from the "public" folder
+app.use(express.static(path.join(__dirname, "public")));
 
-io.on("connection", function (socket) {
-  console.log("A user connected");
+// Handle WebSocket connections
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
 
-//   socket.on('send-location', function (socket)  { //receiving location from one
-//     io.emit("receive-location",{ id: socket.id, ...data}); // sending location to forntend all can see
-//     // console.log('User disconnected');
-//   });
- socket.on('send-location', function (data)  { //receiving location from one
-    io.emit("receive-location",{ id: socket.id, ...data}); // sending location to forntend all can see
-    // console.log('User disconnected');
+  // Listen for location data from a client
+  socket.on('send-location', (data) => {
+    io.emit("receive-location", { id: socket.id, ...data }); // Broadcast location data to all clients
   });
 
-
-socket.on("disconnect", function() {
-//     console.log('message: ' + msg);
-     io.emit("user-disconnected", socket.id); // Broadcast the message to everyone
-   });
+  // Handle user disconnection
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+    io.emit("user-disconnected", socket.id); // Notify all clients about the disconnection
+  });
 });
 
-
-app.get("/",function(req,res){
-    res.render("index")
-})
-
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+// Define routes
+app.get("/", (req, res) => {
+  res.render("index"); // Render the "index.ejs" template
 });
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 
 
 
